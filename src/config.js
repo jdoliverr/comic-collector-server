80706a156c82ca03517a650952737bde8f4e5ad8
Jon <jdog992@gmail.com>
2020-07-01 15:55:45 -0400

    cleaning

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgreql://comic_admin@localhost/comic-collector',
-    TEST_DB_URL: process.env.TEST_DB_URL || 'postgreql://comic_admin@localhost/comic-collector-test',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgreql://comic_admin@localhost/comic-collector',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgreql://comic_admin@localhost/comic-collector-test',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   }
